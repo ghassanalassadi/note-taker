be74b11aa99e1a38fbe7f3e8ccbdd883b5fdae99
ghassan <ghassanalassadi@outlook.com>
2022-07-30 13:43:39 +1000

    started posting notes

--- server.js
+++ server.js
@@ -1,48 +1,53 @@
+const { json } = require("express");
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
-const savedNotes = require("./db/db.json");
 const uuid = require("./helpers/uuid");
 
-const PORT = 3001;
+const PORT = 3002;
 const app = express();
 
 // set up middleware
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
 
+// api routes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
-})
+});
 
-app.post('/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
     const {title, text} = req.body;
     if (title && text) {
         const newNote = {
             title,
             text,
-            note_id: uuid()
+            note_id: uuid(),
         };
 
-        fs.readFile(savedNotes, 'utf-8', (err, data) => {
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
             if (err) {
                 console.error(err);
             } else {
                 const parsedNotes = JSON.parse(data);
                 parsedNotes.push(newNote);
 
-                fs.writeFile(savedNotes, JSON.stringify(parsedNotes, null, 2));
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (writeErr) => writeErr ? console.error(writeErr) : console.info('Successfully added note!'));
             }
         })
 
         const response = {
             status: 'success',
-            body: newNote
+            body: newNote,
         };
 
         console.log(response);
@@ -50,8 +55,11 @@ app.post('/notes', (req, res) => {
     } else {
         res.status(400).json('Error posting note')
     }
-})
+});
 
+app.delete('/notes/api/:note_id', (req, res) => {
+    
+});
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
